a671c22e7444d3e3c5500ba0cf11a381f134bcde
Kamaldeep Srinath <kamaldeep.tumkur@gmail.com>
2020-10-13 14:51:17 +0530

    Initial constructs of storing the blob to indexeddb.
    Signed-off-by: Kamaldeep Srinath <kamaldeep.tumkur@gmail.com>

--- js/filestorage.js
+++ js/filestorage.js
@@ -1,9 +1,11 @@
 'use strict';
 import * as logstring from './logstring.js';
+import {exportJsonBlob} from "./main";
 
 const FILE_EXT = ".json";
 const FILENAME_PREFIX = "export_";
 const OBJECT_STORE_NAME = "speedtests";
+let idbstore = null;
 
 let checkForIndexedDB = function () {
   return (indexedDB !== undefined);
@@ -14,9 +16,13 @@ let storeFileName = function (testID) {
 }
 
 let createStore = function (storename, version = 1) {
+
+  if (idbstore !== null)
+    return;
+
   let timestr = new Date();
 
-  let idbstore = indexedDB.open(storename, version, function (upgradedb) {
+  idbstore = indexedDB.open(storename, version, function (upgradedb) {
     console.log(logstring.speedTestLogString(timestr) +
       'Creating a new object store: ' + storename + '\n' + 'version: ' + version);
     if (!upgradedb.objectStoreNames.contains(OBJECT_STORE_NAME)) {
@@ -25,16 +31,41 @@ let createStore = function (storename, version = 1) {
   });
 
   idbstore.onsuccess = function () {
-    let iDB = idbstore.result;
     console.log(logstring.speedTestLogString(timestr) + 'Indexed DB store creation complete with name: ', storename);
   }
 
   idbstore.onerror = function () {
     console.error(logstring.speedTestLogString(timestr) + ' Error creating the indexedDB data store with name: ' + OBJECT_STORE_NAME);
-    return null;
   }
-
-  return idbstore;
 }
 
+let saveContentsToStoreFile = function(jsonBlob) {
+  if (!checkForIndexedDB()) {
+    console.error(logstring.speedTestLogString(new Date()) + "IndexedDB is not supported in the client");
+    return;
+  }
 
+  // Create the object store first.
+  createStore('SpeedTest');
+
+  let iDB = idbstore.result;
+  console.log(iDB);
+
+  let iDBtx = iDB.transaction([OBJECT_STORE_NAME], 'readwrite');
+  let txstore = iDBtx.objectStore(OBJECT_STORE_NAME);
+  let storeItem = exportJsonBlob;
+  let addrequest = txstore.add(storeItem)
+
+  addrequest.onsuccess  = function() {
+    console.log(logstring.speedTestLogString(new Date()) + " Finished adding record to the file.");
+    iDBtx.stop();
+  }
+
+  addrequest.onerror = function () {
+    console.error(logstring.speedTestLogString(new Date()) + " Error adding the store item.");
+    iDBtx.stop();
+  }
+}
+
+// Consider not exporting the storeFileName.
+export {storeFileName, createStore, saveContentsToStoreFile};
--- js/main.js
+++ js/main.js
@@ -2,113 +2,125 @@
 
 import * as declarations from "./declarations.js";
 import * as logstring from "./logstring.js";
+import * as filestorage from "./filestorage.js";
 
 let testInstance = null;
 let testRequest = null;
+let exportJsonBlob = null;
 
 let createNewTest = function () {
-  testInstance = declarations.createNewTest();
-  return testInstance;
+    testInstance = declarations.createNewTest();
+    return testInstance;
 };
 
 let startSpeedTest = function () {
-  testRequest = declarations.startSpeedTest(testInstance);
+    testRequest = declarations.startSpeedTest(testInstance);
 }
 
 let stopSpeedTest = function (iterCount) {
-  declarations.stopSpeedTest(testInstance);
+    declarations.stopSpeedTest(testInstance);
 
-  // Dispatch the event to abort download.
-  testRequest.dispatchEvent(new ProgressEvent("abort"));
+    // Dispatch the event to abort download.
+    testRequest.dispatchEvent(new ProgressEvent("abort"));
 
 }
 
 let resetSpeedTest = function (iterCount) {
-  // See if save/export options need to be enabled through a modal here.
-  // The testInstance needs to be saved/exported as a JSON.
-  // Save the number of iterations for the current test.
+    // See if save/export options need to be enabled through a modal here.
+    // The testInstance needs to be saved/exported as a JSON.
+    // Save the number of iterations for the current test.
 
-  // Then reset the test instance.
-  // Also handles creating a new test instance.
-  declarations.resetSpeedTest(testInstance);
+    // Then reset the test instance.
+    // Also handles creating a new test instance.
+    declarations.resetSpeedTest(testInstance);
 
-  // Dispatch the event before resetting download.
-  testRequest.dispatchEvent(new ProgressEvent("abort"));
+    // Dispatch the event before resetting download.
+    testRequest.dispatchEvent(new ProgressEvent("abort"));
 
-  // Reset with a new test.
-  createNewTest();
+    // Reset with a new test.
+    createNewTest();
 }
 
 let downloadURL = function () {
-  return declarations.downloadURL;
+    return declarations.downloadURL;
 }
 
 let getProgressPercentageInt = function () {
-  return Math.floor(declarations.getProgressPercentage(testInstance));
+    return Math.floor(declarations.getProgressPercentage(testInstance));
 }
 
 let getProgressPercentageFloat = function () {
-  return declarations.getProgressPercentage(testInstance);
+    return declarations.getProgressPercentage(testInstance);
 }
 
 let setProgressPercentage = function (downloadedSize) {
-  declarations.setProgressPercentage(testInstance, downloadedSize);
+    declarations.setProgressPercentage(testInstance, downloadedSize);
 }
 
 let getSpeedTestResults = function (iteration = -1) {
-  let results = declarations.getSpeedTestResults(testInstance, iteration);
-  return results;
+    let results = declarations.getSpeedTestResults(testInstance, iteration);
+    return results;
 
 }
 
 let mapToObjJSON = function (inputmap) {
-  let mapObj = {};
-  let timestr = new Date();
-  mapObj.testID = testInstance.get("testID");
+    let mapObj = {};
+    let timestr = new Date();
+    mapObj.testID = testInstance.get("testID");
 
-  try {
-    for (let [mapkey, mapval] of inputmap) {
-      mapObj.mapkey = mapval;
+    try {
+        for (let [mapkey, mapval] of inputmap) {
+            mapObj.mapkey = mapval;
+        }
+
+        return mapObj;
+    } catch (e) {
+        console.error(logstring.speedTestLogString(timestr) + "Map to JSON export error");
     }
-
-    return mapObj;
-  } catch (e) {
-    console.error(logstring.speedTestLogString(timestr) + "Map to JSON export error");
-  }
 }
 
 let getAllSpeedTestResultsAsJSON = function () {
-  let iterations = testInstance.get("iterations");
-  let iteration = 1;
-  let jsonBlob = null;
+    let iterations = testInstance.get("iterations");
+    let iteration = 1;
 
-  while (iteration <= iterations) {
-    let testresults = declarations.getSpeedTestResults(testInstance, iteration);
+    while (iteration <= iterations) {
+        // let testresults = declarations.getSpeedTestResults(testInstance, iteration);
+        let testresults = testInstance.get("testResults");
 
-    // Create and populate the file.
-    let jsonFile = new File(["export"], "export.json", {type: 'text/plain',});
+        // Create and populate the file.
+        let jsonFile = new File(["export"], "export.json", {type: 'text/plain',});
 
-    let jsonContents = mapToObjJSON(testresults);
-    console.log(jsonContents);
-    jsonBlob = new Blob([JSON.stringify(jsonContents, null, 2)], {type: 'application/json'});
-    iteration++;
-  }
+        let jsonContents = mapToObjJSON(testresults);
+        console.log(jsonContents);
+        exportJsonBlob = new Blob([JSON.stringify(jsonContents, null, 2)], {type: 'application/json'});
+        iteration++;
+    }
 
-  return jsonBlob;
+    return exportJsonBlob;
+}
+
+let saveContentsToFile = function () {
+    let ftestID = testInstance.get("testID");
+    let filename = filestorage.storeFileName(ftestID);
+    console.log(logstring.speedTestLogString(new Date()) + "Export Filename: " + filename);
+
+    filestorage.saveContentsToStoreFile(exportJsonBlob);
 
 }
 
 export {
-  createNewTest,
-  startSpeedTest,
-  stopSpeedTest,
-  resetSpeedTest,
-  downloadURL,
-  getProgressPercentageInt,
-  getProgressPercentageFloat,
-  setProgressPercentage,
-  getSpeedTestResults,
-  getAllSpeedTestResultsAsJSON,
-  testInstance,
-  testRequest
+    createNewTest,
+    startSpeedTest,
+    stopSpeedTest,
+    resetSpeedTest,
+    downloadURL,
+    getProgressPercentageInt,
+    getProgressPercentageFloat,
+    setProgressPercentage,
+    getSpeedTestResults,
+    getAllSpeedTestResultsAsJSON,
+    saveContentsToFile,
+    testInstance,
+    testRequest,
+    exportJsonBlob
 };
